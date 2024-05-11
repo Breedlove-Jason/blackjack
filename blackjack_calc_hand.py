32ceb73d65063c5d5cdd2310dc4e31d2a32dc38b
Breedlove-Jason <j@jasonbreedlove.com>
2024-05-11 16:00:23 -0700

    finish blackjack_calc_hand

--- blackjack_calc_hand.py
+++ blackjack_calc_hand.py
@@ -1,1 +1,21 @@
+from blackjack_deck import *
 
+deck = Deck()
+deck.shuffle()
+
+player = Hand()
+dealer = Hand()
+
+for i in range(2):
+    player.add_card(deck.deal())
+    dealer.add_card(deck.deal())
+
+    print(f"Player: {player.cards}")
+    print(f"Dealer: {dealer.cards}")
+
+    player.add_card(deck.deal())
+
+    print(f"Player: {player.cards}")
+
+    player.calc_hand()
+    dealer.calc_hand()
--- blackjack_pygame.py
+++ blackjack_pygame.py
@@ -3,6 +3,7 @@ from blackjack_deck import *
 from constants import *
 import sys
 import time
+
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -13,3 +14,27 @@ pygame.display.set_caption("Blackjack")
 gameDisplay.fill(background_color)
 pygame.draw.rect(gameDisplay, gray, pygame.Rect(0, 0, 250, 700))
 
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def end_text_objects(text, font, color):
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
+
+
+play_blackjack = Play()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        button("Deal", 30, 100, 150, 50, light_slat, dark_slat, play_blackjack.deal)
+        button("Hit", 30, 200, 150, 50, light_slat, dark_slat, play_blackjack.hit)
+        button("Stand", 30, 300, 150, 50, light_slat, dark_slat, play_blackjack.stand)
+        button("EXIT", 30, 500, 150, 50, light_slat, dark_red, play_blackjack.exit)
+
+    pygame.display.flip()
